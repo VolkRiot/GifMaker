eb9d533bb0d81d58d8ca316c25c85fd271200053
Mikhail Metrikin <metrikin@gmail.com>
2017-03-10 21:53:12 -0800

    Heroku demands https

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -39,7 +39,7 @@ function createImg(imgObj) {
 
 function postAjaxObject(doThis, search, numItems) {
   $.ajax({
-    url: 'http://api.giphy.com/v1/gifs/search?q=' + search + '&api_key=dc6zaTOxFJmzC&offset='
+    url: 'https://api.giphy.com/v1/gifs/search?q=' + search + '&api_key=dc6zaTOxFJmzC&offset='
     + Math.floor(Math.random() * 3) * 25 + '&rating=PG-13',
     method: "GET"
   }).done(function (response) {
